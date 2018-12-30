c80ccb75dfa78436f659b97d475c5fc2849789f2
Linus Lee <linus@thesephist.com>
2018-12-30 01:54:46 -0500

    screenshots: add better error handling when Puppeteer fails to render

--- src/screenshot.js
+++ src/screenshot.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const puppeteer = require('puppeteer');
 
 const WIDTH = 1024;
@@ -14,27 +16,45 @@ const screenshotReady = async () => {
     });
 }
 
-const getScreenshot = async (url, filePath) =>  {
-
-    const page = await browser.newPage();
-    page.setViewport({
-        width: WIDTH,
-        height: HEIGHT,
-    });
-
-    await page.goto(url);
-    await page.screenshot({
-        path: filePath,
-        type: 'jpeg',
-        quality: 50,
-        clip: {
-            x: 0,
-            y: 0,
+const getScreenshot = async (url, filePath) => {
+    try {
+        const page = await browser.newPage();
+        page.setViewport({
             width: WIDTH,
             height: HEIGHT,
-        }
-    });
-    await page.close();
+        });
+
+        await page.goto(url);
+        await page.screenshot({
+            path: filePath,
+            type: 'jpeg',
+            quality: 50,
+            clip: {
+                x: 0,
+                y: 0,
+                width: WIDTH,
+                height: HEIGHT,
+            }
+        });
+        await page.close();
+    } catch (e) {
+        console.error('Error rendering preview', e);
+        return new Promise((res, rej) => {
+            fs.readFile('./assets/error.jpg', (err, data) => {
+                if (err) {
+                    console.log('Error on attempting to copy error file', err);
+                    rej(err);
+                }
+                fs.writeFile(filePath, data, (err) => {
+                    if (err) {
+                        console.log('Error on attempting to write error file', err);
+                        rej(err);
+                    }
+                    res();
+                });
+            });
+        });
+    }
 }
 
 module.exports = {
